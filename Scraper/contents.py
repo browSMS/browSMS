976ea98678cc48839adad6c15b9c7ee5019be617
deinosaur <codymckee31@gmail.com>
2016-10-16 17:15:56 -0400

    lmao

--- Scraper/contents.py
+++ Scraper/contents.py
@@ -6,14 +6,15 @@
 
 from lxml import html
 import requests
+import re
 
 
 def parsecontent(tree):
 
     #p = tree.xpath('//*[self::p or self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6]/text()')
     p = tree.xpath('//*/text()')
-    p = [x.strip() for x in p if x.strip()]
+    p = ''.join(p)
+    p = re.sub(r'\n\s+\n\s+\n\s+', '\n', p)
     print(type(p))
-    print(str(p))
-    for x in p:
-        print(x)
+    print(p)
+    return p
