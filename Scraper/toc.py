0b7e3c0045abe0bb7986a2ec9d5fe20f70bedb3b
aaronj1 <aaronj1@cs.washington.edu>
2016-10-16 13:18:51 -0700

    Allows testing of toc.py

--- Scraper/toc.py
+++ Scraper/toc.py
@@ -9,11 +9,31 @@ show more: show more images or menus
 '''
 
 from lxml import html
+import pprint
 import requests
 import re
 
-def menu(url):
-    page = requests.get(url)
+def menu(tree):
+    possibleMenus = [
+        tree.find_class('navbar'),
+        tree.find_class('nav'),
+        tree.find_class('menubar'),
+        tree.find_class('menu'),
+        tree.find_class('menu_item'),
+        tree.find_class('nav-flyout__container')
+    ]
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(possibleMenus)
+
+    for possibleMenu in possibleMenus:
+        if len(possibleMenu) != 0:
+            print('hey')
+
+    print(possibleMenus)
+
+    return possibleMenus
+
 
 def search():
     page = requests.get("http://www.facebook.com")
