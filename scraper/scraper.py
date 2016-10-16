ae19b031cd96e9b479fe471430e62a5d3be8d2c2
deinosaur <codymckee31@gmail.com>
2016-10-16 04:13:31 -0400

    scraper update

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,22 +6,20 @@ Uses `parser.py` to parse salient data from the webpage.
 Passes back the data to the messaging service for formatting and sending to the client.
 """
 from lxml import html
+from contents import parsecontent
 import requests
 
 def request():
 	
-	page = requests.get("http://americaisthegreatestcountry.weebly.com/")
+	page = requests.get("http://deinosaur.github.io/cody-go-fish/")
 	print(page.status_code)
 	print(page.headers['content-type'])
 	print(page.encoding)
 
 	if (page.status_code == 200):
 		tree = html.fromstring(page.content)
-
-		p = tree.xpath('//a/text()')
-		print(type(p))
-		for item in p:
-			print(str(item).strip())
+	print(type(tree))
+	parsecontent(tree)
 
 
 request()
