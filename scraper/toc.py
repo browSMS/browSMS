24665e0b598363c12ae31c397cb254a24e18112f
Peter Li <peterli97@gmail.com>
2016-10-16 01:26:50 -0700

    framework for table of contents

---
+++ scraper/toc.py
@@ -0,0 +1,28 @@
+'''
+Returns the table of contents of the webpage
+
+Contains:
+menu: maps between text and url to go to
+login<string user, string pass>: accepts a user and password
+back: returns the most recent webpage that was cached
+# forward: EXTRA FEATURE, go forward one page
+search<string query>: pass in this string into the search bar
+show more: show more images or menus
+
+'''
+
+from lxml import html
+
+def menu():
+
+def login():
+
+def back():
+
+#def forward():
+
+def search():
+
+def moreImages():
+
+def moreMenus():
