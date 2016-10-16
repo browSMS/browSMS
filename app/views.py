1a2e38ccc2dc2b4a3f225bcc37b695ba27e7cac6
saulmil <mileshsaul@yahoo.com>
2016-10-16 12:58:03 -0700

    Changed messenger and views

--- app/views.py
+++ app/views.py
@@ -1,30 +1,32 @@
 import Scraper
+from Messenger import SMSMessenger
 from flask import request, redirect, session
 from app import app
-import twilio.twiml
+#import twilio.twiml
+
+messenger = SMSMessenger()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     message = ''
-    resp = twilio.twiml.Response()
     if request.get('body')[:4] == 'http':
         url = request.get('body')
         message = navigate(url)
     else:
-    	if request.get('To') in session:
-    	    url = session[request.get('To')]
+        if request.get('To') in session:
+            url = session[request.get('To')]
             options = session[url] 
             str_options = valid_options(options)
             if not request.get('body') in str_options:
                 message = 'Please enter a valid option:\n'
                 message += make_options(options)
             else:
-                url = options[int(request.get('body'))][1]
+                url = options[int(request.get('body')))[1]
                 message = navigate(url)
-	else:
+        else:
             message = 'Please enter a valid url'
-    resp.message(message)
-    return str(resp)
+    messenger.send_message(body=message, to=request.get('From'))
+    return 'Hey' 
 
 def valid_options(options):
     for i in xrange(len(options)):
@@ -49,4 +51,3 @@ def navigate(url):
     message = 'You are now at: ' + url 
     message += make_options(session[url])
     return message
-
