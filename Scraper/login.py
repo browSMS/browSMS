6b50f88c16681e999d1e6dc8a9cfec1c0f4ee268
Peter Li <peterli97@gmail.com>
2016-10-16 11:29:52 -0700

    renamed scraper

---
+++ Scraper/login.py
@@ -0,0 +1,40 @@
+'''
+Configures login
+
+login<string user, string pass>: accepts a user and password
+'''
+
+def login(url, u, p):
+    page = requests.get(url)
+    if (page.status_code == 200):
+        #tree = html.fromstring(page.content)
+        #input_text = tree.xpath('//input/text()')
+        #for item in input_text:
+        #    print(item)
+        decoded = page.content.decode('UTF-8')
+        post_url = re.search("<form[^>]*method=('|\")post('|\")[^>]*(\/)?>", decoded)
+        if post_url != None:
+            post_url = re.split("action=('|\")", post_url.group(0))
+            for item in post_url:
+                if item.startswith('http'):
+                    post_url = re.split("\" ", item)
+                    post_url = post_url[0]
+                    break
+            #print(post_url[0])
+        else:
+            return None
+        user = re.search("<input[^>]*type=('|\")(text|email)('|\")[^>]*(\/)?>", decoded)
+        password = re.search("<input[^>]*type=('|\")password('|\")[^>]*(\/)?>", decoded)
+        if user != None and password != None:
+            # print(user.group(0))
+            # print(password.group(0))
+            # print(post_url)
+            user = user.group(0)
+            password = password.group(0)
+            payload = {'email': u, 'pass': p}
+            r = requests.post(post_url, data=payload)
+            print(r.text)
+        else:
+            return None
+
+login("http://www.facebook.com", "peterli97@gmail.com", "Handfoot13")
--- scraper/toc.py
+++ scraper/toc.py
@@ -3,7 +3,6 @@ Returns the table of contents of the webpage
 
 Contains:
 menu: maps between text and url to go to, returns best image too
-login<string user, string pass>: accepts a user and password
 search<string query>: pass in this string into the search bar
 show more: show more images or menus
 
@@ -16,42 +15,6 @@ import re
 def menu(url):
     page = requests.get(url)
 
-
-def login(url, u, p):
-    page = requests.get(url)
-    if (page.status_code == 200):
-        #tree = html.fromstring(page.content)
-        #input_text = tree.xpath('//input/text()')
-        #for item in input_text:
-        #    print(item)
-        decoded = page.content.decode('UTF-8')
-        post_url = re.search("<form[^>]*method=('|\")post('|\")[^>]*(\/)?>", decoded)
-        if post_url != None:
-            post_url = re.split("action=('|\")", post_url.group(0))
-            for item in post_url:
-                if item.startswith('http'):
-                    post_url = re.split("\" ", item)
-                    post_url = post_url[0]
-                    break
-            #print(post_url[0])
-            payload = {'email': u, 'pass': p}
-        else:
-            return None
-        user = re.search("<input[^>]*type=('|\")(text|email)('|\")[^>]*(\/)?>", decoded)
-        password = re.search("<input[^>]*type=('|\")password('|\")[^>]*(\/)?>", decoded)
-        if user != None and password != None:
-            # print(user.group(0))
-            # print(password.group(0))
-            # print(post_url)
-            user = user.group(0)
-            password = password.group(0)
-            r = requests.post(post_url, data=payload)
-            print(r.text)
-        else:
-            return None
-
-
-
 def search():
     page = requests.get("http://www.facebook.com")
     print(page.content)
@@ -63,5 +26,3 @@ def moreImages():
 
 def moreMenus():
     pass
-
-login("http://www.facebook.com", "peterli97@gmail.com", "Handfoot13")
