ffadbde5dd80864e58fc5d2f1381894049469aae
aaronj1 <aaronj1@cs.washington.edu>
2016-10-16 02:14:50 -0700

    Adds simple HTML filtering

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,10 +6,11 @@ Uses `parser.py` to parse salient data from the webpage.
 Passes back the data to the messaging service for formatting and sending to the client.
 """
 from lxml import html
+from lxml import etree
 from contents import parsecontent
 import requests
 
-def request():
+def navigate(uid, url):
 	
 	page = requests.get("http://deinosaur.github.io/cody-go-fish/")
 	print(page.status_code)
@@ -18,8 +19,19 @@ def request():
 
 	if (page.status_code == 200):
 		tree = html.fromstring(page.content)
-	print(type(tree))
-	parsecontent(tree)
 
+		# Strip head from tree
+		for head in tree.xpath('//head'):
+			head.getparent().remove(head)
 
-request()
+		print(etree.tostring(tree, pretty_print=True))
+
+		# TODO: call toc
+
+		# Generate contents from cleaned tree
+		parsecontent(tree)
+
+def getSessionData(uid):
+	#TODO: gets data about the settions from the database, only used internally for navigate
+
+navigate(2, 'www.google.com')
